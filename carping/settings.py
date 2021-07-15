8691aaf08714e404a64b1c0bcab103a455591d7b
Chanjongp <pcj0198@daum.net>
2021-07-16 00:40:16 +0900

    Feat : Static Root 추가

--- carping/settings.py
+++ carping/settings.py
@@ -126,7 +126,10 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
