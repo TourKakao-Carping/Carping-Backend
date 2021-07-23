e55a72944628029db4ae0c3aaa916280e574aab3
julia-ing <ywchoi0625@gmail.com>
2021-07-24 05:04:29 +0900

    Feat : 구글로그인 기본 세팅

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from accounts import views
 urlpatterns = [
     path('kakao/login/',
          views.KakaoLoginView.as_view(), name='kakao_login'),
+    path('google/login/',
+         views.GoogleLoginView.as_view(), name='google_login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from allauth.socialaccount.providers.kakao import views as kakao_view
+from allauth.socialaccount.providers.google import views as google_view
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
 # Create your views here.
@@ -16,3 +17,9 @@ class KakaoLoginView(SocialLoginView):
     adapter_class = kakao_view.KakaoOAuth2Adapter
     # client_class = OAuth2Client
     # callback_url = 'http://localhost:8000/accounts/kakao/login'
+
+
+class GoogleLoginView(SocialLoginView):
+    adapter_class = google_view.GoogleOAuth2Adapter
+    # client_class = OAuth2Client
+    # callback_url = 'http://localhost:8000/accounts/google/login'
