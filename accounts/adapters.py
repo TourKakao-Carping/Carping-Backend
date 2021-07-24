1aca405c2978e55b28a36683f55e2c71ac46a232
Chanjongp <pcj0198@daum.net>
2021-07-24 14:26:18 +0900

    Feat : KAKAO Login 추가

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -1,8 +1,25 @@
 
+from django.http.response import JsonResponse
+import requests
+from accounts.models import Profile
+from allauth.socialaccount.models import SocialAccount
 from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
 
 
 class CustomKakaoOAuth2Adapter(KakaoOAuth2Adapter):
     def complete_login(self, request, app, token, **kwargs):
-        print("!1")
-        return super().complete_login(request, app, token, **kwargs)
+        headers = {"Authorization": "Bearer {0}".format(token.token)}
+        resp = requests.get(self.profile_url, headers=headers)
+        resp.raise_for_status()
+        extra_data = resp.json()
+        uid = extra_data.get("id")
+        if SocialAccount.objects.filter(uid=uid).exists():
+            # return JsonResponse({"error_code": "USER_EXISTS", "message": "exists"})
+            pass
+        else:
+            kakao_account = extra_data.get("kakao_account")
+            profile = kakao_account.get("profile")
+            nickname = profile.get("nickname")
+            profile_image = profile.get("profile_image_url")
+            Profile.objects.create(nickname=nickname, image=profile_image,)
+        return self.get_provider().sociallogin_from_response(request, extra_data)
