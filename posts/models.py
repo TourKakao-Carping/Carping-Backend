6253fba0494c927e656f058c37a4eef11f33839c
julia-ing <ywchoi0625@gmail.com>
2021-08-07 01:26:28 +0900

    Feat: add Post, EcoCarping, Share models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+from accounts.models import User
+from bases.models import Base
+from camps.models import CampSite
+
+
+class Post(Base):
+    user = models.ForeignKey(User, on_delete=CASCADE, related_name="post", null=True)
+    campsite = models.ForeignKey(CampSite, on_delete=CASCADE, related_name="post", null=True)
+    title = models.CharField(max_length=100, null=False)
+    text = models.TextField()
+    views = models.IntegerField(default=0)
+
+
+class EcoCarping(Base):
+    user = models.ForeignKey(User, on_delete=CASCADE, related_name="eco", null=True)
+    latitude = models.FloatField(null=False)
+    longitude = models.FloatField(null=False)
+    image = models.CharField(max_length=100, null=True)
+    title = models.CharField(max_length=100, null=False)
+    text = models.TextField()
+
+
+class Share(Base):
+    user = models.ForeignKey(User, on_delete=CASCADE, related_name="share", null=True)
+    latitude = models.FloatField(null=False)
+    longitude = models.FloatField(null=False)
+    image = models.CharField(max_length=100, null=True)
+    title = models.CharField(max_length=100, null=False)
+    text = models.TextField()
