419c754cbc33366e6ffaa40d2ca8e3e4cbcbcd2a
julia-ing <ywchoi0625@gmail.com>
2021-08-07 02:04:37 +0900

    Feat: register models in admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from posts.models import Post, EcoCarping, Share
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(EcoCarping)
+admin.site.register(Share)
