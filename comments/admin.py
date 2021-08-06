1f45766292f0f0c2546dec60dc17faf83955212d
julia-ing <ywchoi0625@gmail.com>
2021-08-07 01:47:27 +0900

    Chore: admin test

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from comments.models import Review, Comment, Like, BookMark
 
-# Register your models here.
+admin.site.register(Review)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(BookMark)
