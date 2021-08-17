c1c2f0e83b177869c0e32be142d9038f37223e97
julia-ing <ywchoi0625@gmail.com>
2021-08-17 19:34:56 +0900

    Feat: modify response form in AutoCampViewSet

--- camps/viewsets.py
+++ camps/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, CreateModelMixin
 from rest_framework.viewsets import GenericViewSet
 
+from bases.response import APIResponse
 from camps.models import AutoCamp
 from camps.serializers import AutoCampSerializer
 
@@ -8,3 +9,14 @@ from camps.serializers import AutoCampSerializer
 class AutoCampViewSet(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, CreateModelMixin, GenericViewSet):
     serializer_class = AutoCampSerializer
     queryset = AutoCamp.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        response = APIResponse(False, '')
+        try:
+            ret = super(AutoCampViewSet, self).retrieve(request)
+
+            response.success = True
+            return response.response(data=[ret.data], status=200)
+        except Exception as e:
+            response.code = "NOT_FOUND"
+            return response.response(data=str(e), status=404)
