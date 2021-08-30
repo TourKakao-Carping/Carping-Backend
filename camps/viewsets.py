90ac05f5784cef1e17cd87911258c171591014bd
Chanjongp <pcj0198@daum.net>
2021-08-30 21:14:33 +0900

    Fix: Error

--- camps/viewsets.py
+++ camps/viewsets.py
@@ -29,7 +29,7 @@ class AutoCampViewSet(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, C
             ret = super(AutoCampViewSet, self).create(request)
         except Exception as e:
             response.code = "NOT_FOUND"
-            return response.response(data=str(e), status=200)
+            return response.response(data=[str(e)], status=200)
 
         response.success = True
         return response.response(data=[ret.data], status=200)
