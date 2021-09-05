9ff25e160798d46130400436bde04627f26be4ed
julia-ing <ywchoi0625@gmail.com>
2021-09-06 02:45:49 +0900

    Feat: 차박지 수정

--- camps/viewsets.py
+++ camps/viewsets.py
@@ -46,15 +46,31 @@ class AutoCampViewSet(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, C
             response.code = status.HTTP_404_NOT_FOUND
             return response.response(error_message=str(e))
 
-    def partial_update(self, request, *args, **kwargs):
+    def update(self, request, *args, **kwargs):
         response = APIResponse(success=False, code=400)
+        auto = self.get_object()
         lat = request.data.get('latitude')
         lon = request.data.get('longitude')
+        image1 = request.data.get('image1')
+        image2 = request.data.get('image2')
+        image3 = request.data.get('image3')
+        image4 = request.data.get('image4')
+
         try:
+            if image1 is None:
+                auto.image1.delete()
+            if image2 is None:
+                auto.image2.delete()
+            if image3 is None:
+                auto.image3.delete()
+            if image4 is None:
+                auto.image4.delete()
+
             if check_str_digit(lat) and check_str_digit(lon):
                 float(lat)
                 float(lon)
-            ret = super(AutoCampViewSet, self).partial_update(request)
+
+            ret = super(AutoCampViewSet, self).update(request)
 
             response.success = True
             response.code = 200
