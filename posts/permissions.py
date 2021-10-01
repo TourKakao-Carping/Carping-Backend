8b79535c00856418c231285e522ea7973af9dc16
Chanjongp <pcj0198@daum.net>
2021-10-01 22:50:33 +0900

    Fix : 권한수정중

--- posts/permissions.py
+++ posts/permissions.py
@@ -4,6 +4,9 @@ from rest_framework import permissions
 class UserPostAccessPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        print(view.serializer_class)
+        string = request.META['QUERY_STRING']
+        print(request.data)
         user = request.user
 
-        return super().has_permission(request, view)
+        return True
