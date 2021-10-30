1732d86dd1f805d8f7025df083a8f36e1006eca9
Chanjongp <pcj0198@daum.net>
2021-10-30 13:02:16 +0900

    Fix : Add Views from Admin Post

--- carping/settings/base.py
+++ carping/settings/base.py
@@ -162,7 +162,7 @@ REST_FRAMEWORK = {
 REST_USE_JWT = True
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=365),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
