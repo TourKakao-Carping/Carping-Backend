9887388dcbdaba503694e0d837576a7ebad6df94
Chanjongp <pcj0198@daum.net>
2021-10-31 21:31:07 +0900

    Fix : change static root image

--- carping/settings/base.py
+++ carping/settings/base.py
@@ -140,6 +140,8 @@ SITE_ID = 1
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'images'),
+
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
